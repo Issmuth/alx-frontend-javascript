adc3a3da8009b3b29d3d113a2786634f16cd9014
issmuth <ismael.mdev@gmail.com>
2024-07-12 23:49:50 +0300

    done

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array) {
-    const value = idx;
+  for (const value of array) {
     array.push(appendString + value);
   }
 
